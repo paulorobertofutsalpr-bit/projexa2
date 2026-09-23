af5c286cc01b1eb01c0268dc9eac66a7f9eaf66e
paulorobertofutsalpr-bit <pauloroberto.futsal.pr@gmail.com>
2026-09-23 18:39:46 -0300

    Rename 2-0013_shallow_tiger_shark.sql to 0013_shallow_tiger_shark.sql

---
+++ db/migrations/0013_shallow_tiger_shark.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "companies" ADD COLUMN "subscription_price_cents" integer DEFAULT 3900 NOT NULL;
--- db/migrations/2-0013_shallow_tiger_shark.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "companies" ADD COLUMN "subscription_price_cents" integer DEFAULT 3900 NOT NULL;
